31928fcf3e9dc3a68f3b528c7e13b73489882ad6
Mike <82026751+MikeNoCap@users.noreply.github.com>
2022-09-08 14:25:30 +0200

    unndo

--- frontend/components/header.jsx
+++ frontend/components/header.jsx
@@ -5,7 +5,7 @@ import styles from '../styles/Header.module.css'
 export default function Header(props) {
     return (
         <div id={styles["topbar"]}>
-            <h1 id={styles["logo-text"]}>Test</h1>
+            <h1 id={styles["logo-text"]}>der Germanator</h1>
         </div>
     );
 }
