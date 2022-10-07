60ac2b2a0edd690874ecdd416ee2d181e4c88c04
Mike <82026751+MikeNoCap@users.noreply.github.com>
2022-10-07 12:41:14 +0200

    a

--- frontend/components/weekSet/pugg.jsx
+++ frontend/components/weekSet/pugg.jsx
@@ -55,7 +55,7 @@ class Pugg extends Component {
             <React.Fragment>
                 <Header />
                 <div id={styles["page"]}>
-                    {<LanguageSelector handler = {this.setLang} /> && true}
+                    {"test" && true}
                 </div>
             </React.Fragment>
         )
