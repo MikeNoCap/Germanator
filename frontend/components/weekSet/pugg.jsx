9db9516150f0990035051d8f023e716e13cc2b28
Mike <82026751+MikeNoCap@users.noreply.github.com>
2022-10-07 12:18:50 +0200

    a

--- frontend/components/weekSet/pugg.jsx
+++ frontend/components/weekSet/pugg.jsx
@@ -1,17 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import styles from "../../styles/Pugg.module.css";
 import Header from "../header.jsx";
 
 
 function LanguageSelector(props) {
+    const [selectedLang, setSelectedLang] = useState("");
+    let germanSelected = "";
+    let norwegianSelected = "";
+    if (selectedLang == "norwegian") {
+        norwegianSelected = " "+styles["selected"]
+    }
+    else if (selectedLang == "german") {
+        germanSelected = " "+styles["selected"]
+    }
     return (
         <div id={styles["lang-select"]}>
             <h1>Jeg vil svare på ...</h1>
             <div id={styles["lang-options"]}>
-                <button id={styles["german"]} className={styles["lang"]}>
+                <button onClick={() => {
+                    setSelectedLang("german");
+                }} id={styles["german"]} className={styles["lang"]+germanSelected}>
                     <h2>Tysk</h2>
                 </button>
-                <button id={styles["norwegian"]} className={styles["lang"]}>
+
+                <button onClick={() => {
+                    setSelectedLang("norwegian");
+                }} id={styles["norwegian"]} className={styles["lang"]+norwegianSelected}>
                     <h2>Norsk</h2>
                 </button>
             </div>
