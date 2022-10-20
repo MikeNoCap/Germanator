96bfee1ad679656dadc2060ae50eff1321b8fb79
Mike <82026751+MikeNoCap@users.noreply.github.com>
2022-10-20 22:24:37 +0200

    a

--- frontend/components/weekSet/pugg.jsx
+++ frontend/components/weekSet/pugg.jsx
@@ -168,10 +168,8 @@ class Pugg extends Component {
                             spellcheck="false" 
                             onChange={this.handleInput} 
                             onKeyPress={this.handleEnter} />
-                            <div id={styles["svar-input-load"]}>
-                                <h3 id={styles["svar-input-correct"]}>Super!</h3>
-                            </div>
-                            
+                            <div id={styles["svar-input-load"]} />
+                            <h3 id={styles["svar-input-correct"]} >Super!</h3>
                         </div>
 
                         <div id={styles["answer-buttons"]}>
